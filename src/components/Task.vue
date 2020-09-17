6aa9b69e45967d59def3fd6651afe55bdfe851aa
拉拉醬 <lala.lee.goo@gmail.com>
2020-09-17 22:13:24 +0800

    完善 Task 元件 加上狀態

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,28 @@
 <!--src/components/Task.vue-->
 <template>
-  <div class="list-item">
-    <input type="text" :readonly="true" :value="this.task.title" />
+  <div :class="taskClass">
+    <label class="checkbox">
+      <input
+        type="checkbox"
+        :checked="isChecked"
+        :disabled="true"
+        name="checked"
+      />
+      <span class="checkbox-custom" @click="$emit('archive-task', task.id)" />
+    </label>
+    <div class="title">
+      <input
+        type="text"
+        :readonly="true"
+        :value="this.task.title"
+        placeholder="Input title"
+      />
+    </div>
+    <div class="actions">
+      <a @click="$emit('pin-task', task.id)" v-if="!isChecked">
+        <span class="icon-star" />
+      </a>
+    </div>
   </div>
 </template>
 
@@ -12,7 +33,19 @@ export default {
     task: {
       type: Object,
       required: true,
-      default: () => ({})
+      default: () => ({
+        id: "",
+        state: "",
+        title: ""
+      })
+    }
+  },
+  computed: {
+    taskClass() {
+      return `list-item ${this.task.state}`;
+    },
+    isChecked() {
+      return this.task.state === "TASK_ARCHIVED";
     }
   }
 };

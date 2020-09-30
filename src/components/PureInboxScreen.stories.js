e5dc55fa1eb7ec77c5792e26d32f356219e1a3c7
拉拉醬 <lala.lee.goo@gmail.com>
2020-10-01 01:02:28 +0800

    Add PureInboxScreen.stories.js

---
+++ src/components/PureInboxScreen.stories.js
@@ -0,0 +1,17 @@
+//src/components/PureInboxScreen.stories.js
+import PureInboxScreen from './PureInboxScreen.vue';
+export default {
+  title: 'PureInboxScreen',
+};
+
+// inbox screen default state
+export const Default = () => ({
+  components: { PureInboxScreen },
+  template: `<pure-inbox-screen/>`,
+});
+
+// inbox screen error state
+export const error = () => ({
+  components: { PureInboxScreen },
+  template: `<pure-inbox-screen :error="true"/>`,
+});
